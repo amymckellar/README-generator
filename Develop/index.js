c6b6421b747bdaa079682caf825237da135f267a
amymckellar <amy_mckellar@yahoo.com>
2023-02-02 12:53:42 -0600

    adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
 
+const { default: inquirer } = require("inquirer");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+    {
+        type: 'input',
+        message: 'What is the Title of your project?'
+        name: 'Title',
+    },
+    {
+        type: 'input',
+        name: 'What is the Description of your project?',
+        message: 'Description',
+    },
+    {   type: 'input',
+        name: 'What are the Installation Instructions?',
+        message: 'Installation',
+    },
+    {   type: 'input',
+        name: 'What are your Usage Instructions?',  
+        message: 'Usage',
+    },
+    {   type: 'input',
+        name: 'What are your Contribution Guidelines?',
+        message: 'Contribution',
+    },
+    {   type: 'input',
+        name: 'What are your Test Instructions?',
+        message: 'Test',
+    },
+    ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
